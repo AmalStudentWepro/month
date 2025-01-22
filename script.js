4f680a8fbd4bd8cf5268226185790a6c0bec2df0
AmalStudentWepro <163731290+AmalStudentWepro@users.noreply.github.com>
2025-01-22 14:08:22 +0300

    First uploud site

---
+++ script.js
@@ -0,0 +1,56 @@
+let month = prompt("Введите номер месяца (1-12):");
+
+switch (month) {
+
+  case '1':
+    alert("В этом месяце 31 день");
+    break;
+
+  case '2':
+    alert("В этом месяце 28 или 29 дней");
+    break;
+
+  case '3':
+    alert("В этом месяце 31 день");
+    break;
+
+  case '4':
+    alert("В этом месяце 30 дней");
+    break;
+
+  case '5':
+    alert("В этом месяце 31 день");
+    break;
+
+  case '6':
+    alert("В этом месяце 30 дней");
+    break;
+
+  case '7':
+    alert("В этом месяце 31 день");
+    break;
+
+  case '8':
+    alert("В этом месяце 31 день");
+    break;
+
+  case '9':
+    alert("В этом месяце 30 дней");
+    break;
+
+  case '10':
+    alert("В этом месяце 31 день");
+    break;
+
+  case '11':
+    alert("В этом месяце 30 дней");
+    break;
+
+  case '12':
+    alert("В этом месяце 31 день");
+    break;
+
+  default:
+    alert("Нет такого месяца");
+    break;
+}
